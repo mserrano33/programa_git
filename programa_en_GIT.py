d030c4ceec8e181afc823d593e36ddadb6277070
mserrano33 <serrano.matias.sebastian@gmail.com>
2020-07-28 06:01:20 -0300

    agregamos archivo programa_en_GIT.py

---
+++ programa_en_GIT.py
@@ -0,0 +1,13 @@
+numero1=input('ingrese primer numero ')
+
+numero2=input('ingrese segundo numero ')
+
+ 
+if numero1<=numero2:
+
+    print('el mayor numero entre', numero1, 'y', numero2, 'es ', numero2)
+
+
+else:
+
+    print('el mayor numero entre', numero1, 'y', numero2, 'es ', numero1)
