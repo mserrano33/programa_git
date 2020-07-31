8e78666e14bfc13c56fe4c5a785a4268da4148f4
mserrano33 <serrano.matias.sebastian@gmail.com>
2020-07-31 05:47:42 -0300

    vocal o consonante

--- programa_en_GIT.py
+++ programa_en_GIT.py
@@ -21,8 +21,19 @@
   #print("El numero mayor es " + str(B))
  #else:
   #print("El numero mayor es " + str(C))
-l = ["a","b","b"]
-[[x,l.count(x)] for x in set(l)]
-[['a', 1], ['b', 2]]
-dict((x,l.count(x)) for x in set(l))
-{'a': 1, 'b': 2}
+#l = ["a","b","b"]
+#[[x,l.count(x)] for x in set(l)]
+#[['a', 1], ['b', 2]]
+#dict((x,l.count(x)) for x in set(l))
+#{'a': 1, 'b': 2}
+
+vocales = ['a','e','i','o','u']
+
+caracter = input('Ingrese un caracter:')
+
+if caracter in vocales:
+
+  print('El caracter %c es una vocal' %caracter)
+else:
+
+  print('El caracter %c es una consonante' %caracter)
