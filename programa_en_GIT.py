2c3c02b19703e1545876e21d8cb2ec5af19fa88e
mserrano33 <serrano.matias.sebastian@gmail.com>
2020-07-31 04:28:54 -0300

    maximo de 3 numeros

--- programa_en_GIT.py
+++ programa_en_GIT.py
@@ -1,13 +1,23 @@
-numero1=input('ingrese primer numero ')
+#numero1=input('ingrese primer numero ')
 
-numero2=input('ingrese segundo numero ')
+#numero2=input('ingrese segundo numero ')
 
  
-if numero1<=numero2:
+#if numero1<=numero2:
 
-    print('el mayor numero entre', numero1, 'y', numero2, 'es ', numero2)
+#    print('el mayor numero entre', numero1, 'y', numero2, 'es ', numero2)
 
 
+#else:
+
+#    print('el mayor numero entre', numero1, 'y', numero2, 'es ', numero1)
+A=int(input("ingrese un numero\n"))
+B=int(input("ingrese otro numero\n"))
+C=int(input("ingrese un nuemero\n"))
+if(A > B and A > C):
+ print("El numero mayor es " + str(A))
 else:
-
-    print('el mayor numero entre', numero1, 'y', numero2, 'es ', numero1)
+ if(B > A and B > C):
+  print("El numero mayor es " + str(B))
+ else:
+  print("El numero mayor es " + str(C))
