fe00460f00ce66bd2553dfcd869cb0cb01d8409d
mserrano33 <serrano.matias.sebastian@gmail.com>
2020-07-31 05:05:35 -0300

    largo de la lista

--- programa_en_GIT.py
+++ programa_en_GIT.py
@@ -11,13 +11,18 @@
 #else:
 
 #    print('el mayor numero entre', numero1, 'y', numero2, 'es ', numero1)
-A=int(input("ingrese un numero\n"))
-B=int(input("ingrese otro numero\n"))
-C=int(input("ingrese un nuemero\n"))
-if(A > B and A > C):
- print("El numero mayor es " + str(A))
-else:
- if(B > A and B > C):
-  print("El numero mayor es " + str(B))
- else:
-  print("El numero mayor es " + str(C))
+#A=int(input("ingrese un numero\n"))
+#B=int(input("ingrese otro numero\n"))
+#C=int(input("ingrese un nuemero\n"))
+#if(A > B and A > C):
+ #print("El numero mayor es " + str(A))
+#else:
+ #if(B > A and B > C):
+  #print("El numero mayor es " + str(B))
+ #else:
+  #print("El numero mayor es " + str(C))
+l = ["a","b","b"]
+[[x,l.count(x)] for x in set(l)]
+[['a', 1], ['b', 2]]
+dict((x,l.count(x)) for x in set(l))
+{'a': 1, 'b': 2}
